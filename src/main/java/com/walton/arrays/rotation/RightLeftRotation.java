9a76bfd1019b8b95a2d4e95616af8a0f05f08892
Walton Goga <waltongoga@gmail.com>
2022-11-28 07:48:37 +0300

    Right rotation

---
+++ src/main/java/com/walton/arrays/rotation/RightLeftRotation.java
@@ -0,0 +1,33 @@
+package com.walton.arrays.rotation;
+
+import java.util.Arrays;
+
+/**
+ * Given an array of integers arr[] of size N and an integer, the task is to rotate the array
+ * elements to the left and right by d positions.
+ */
+public class RightLeftRotation {
+
+  public static void rightRotation(int arr[], int N, int d){
+    int temp[] = new int[N];
+    int tempIndex = 0;
+
+    for(int i = N - d; i < N; i++){
+      temp[tempIndex] = arr[i];
+      tempIndex++;
+    }
+
+    for(int i = 0; i < N - d; i++){
+      temp[tempIndex] = arr[i];
+      tempIndex++;
+    }
+
+    System.out.print(Arrays.toString(temp));
+
+  }
+
+  public static void main(String[] args){
+    rightRotation(new int[]{1,2,3,4,5,6}, 6, 2);
+  }
+
+}
